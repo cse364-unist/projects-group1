b97f14cb2dbf7787c591a1e8b995710459192430
jgyu25 <ingyujang25@unist.ac.kr>
2024-04-23 20:47:44 +0900

    Modify User.java to handle all added properties

--- milestone1/src/main/java/com/example/entity/User.java
+++ milestone1/src/main/java/com/example/entity/User.java
@@ -17,13 +17,23 @@ public class User {
     private int age;
     private int occupation;
     private String zipCode;
+    private int point;
+    private int movieStatus;
+    private int partyRoomStatus;
+    private int hobby;
+    private int favoriteFood;
 
-    public User(int userId, String gender, int age, int occupation, String zipCode){
+    public User(int userId, String gender, int age, int occupation, String zipCode, int point, int movieStatus, int partyRoomStatus, int hobby, int favoriteFood){
         this.userId = userId;
         this.gender = gender;
         this.age = age;
         this.occupation = occupation;
         this.zipCode = zipCode;
+        this.point = point;
+        this.movieStatus = movieStatus;
+        this.partyRoomStatus = partyRoomStatus;
+        this.hobby = hobby;
+        this.favoriteFood = favoriteFood;
     }
 
 }
