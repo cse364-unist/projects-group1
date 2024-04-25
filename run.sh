e74aa7fde228e29f72bb37100683eb4abca688d2
Gyuwon <gyuwon12@unist.ac.kr>
2024-04-25 23:21:41 +0900

    placeID 수정사항-> , 오류 수정

--- run.sh
+++ run.sh
@@ -3,13 +3,13 @@ cd milestone1
 mongod --fork --logpath /var/log/mongodb.log
 mongosh admin --eval "db.createUser({ user: 'user', pwd: 'password', roles: ['userAdminAnyDatabase'] })"
 mongoimport --db=assign1 --collection=movie --authenticationDatabase admin --username user --password password --type=csv --file=data/movies.csv \
-    --fields=movieId.int32\(\),name.string\(\),genre.string\(\),placeId,int32\(\) --columnsHaveTypes 
+    --fields=movieId.int32\(\),name.string\(\),genre.string\(\),placeId.int32\(\) --columnsHaveTypes 
 mongoimport --db=assign1 --collection=rating --authenticationDatabase admin --username user --password password --type=csv --file=data/ratings.csv \
     --fields=userId.int32\(\),movieId.int32\(\),rating.int32\(\),timestamp.string\(\) --columnsHaveTypes 
 mongoimport --db=assign1 --collection=user --authenticationDatabase admin --username user --password password --type=csv --file=data/users.csv \
     --fields=userId.int32\(\),gender.string\(\),age.int32\(\),occupation.int32\(\),zipCode.string\(\),point.int32\(\),movieStatus.int32\(\),latitude.double\(\),longitude.double\(\),hobby.int32\(\),favoriteFood.int32\(\) --columnsHaveTypes
 mongoimport --db=assign1 --collection=place --authenticationDatabase admin --username user --password password --type=csv --file=data/moviePlaces.csv \
-    --fields=placeId.int32\(\),name.string\(\)latitude.double\(\),longitude.double\(\),hobby.int32\(\),favoriteFood.int32\(\) --columnsHaveTypes
+    --fields=placeId.int32\(\),name.string\(\),latitude.double\(\),longitude.double\(\),hobby.int32\(\),favoriteFood.int32\(\) --columnsHaveTypes
 
 mvn package
 java -jar ./target/cse364-project-1.0-SNAPSHOT.jar
