f4450535b2e91bd7673a7aafe2738f166d699909
Gyuwon <gyuwon12@unist.ac.kr>
2024-04-26 18:14:59 +0900

    테스트코드, branch coverage 반영

--- milestone1/src/main/java/com/example/entity/Content.java
+++ milestone1/src/main/java/com/example/entity/Content.java
@@ -2,7 +2,11 @@ package com.example.entity;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Document(collection = "content")
 public class Content {
 
