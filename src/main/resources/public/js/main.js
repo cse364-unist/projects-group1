64ca87042fd63e435e43c06a7f04b5c3b55e573c
Woojin Kim <woojinkim@unist.ac.kr>
2024-06-16 15:33:38 +0900

    Update main.js

--- src/main/resources/public/js/main.js
+++ src/main/resources/public/js/main.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
+    const hobbies = ['Tourism', 'Leisure', 'Cafe', 'Nature', 'Recreation'];
+    const favoriteFoods = ['Korean food', 'Chinese food', 'Japanese food', 'Western food', 'Dessert'];
+
+    // Function to convert bitwise number to human-readable strings
+    function bitwiseToReadable(bits, labels) {
+        let result = [];
+        for (let i = 0; i < labels.length; i++) {
+            if (bits & (1 << i)) {
+                result.push(labels[i]);
+            }
+        }
+        return result.join(' and ');
+    }
+
     // Load all movies on page load
     $.get('/places/movies', function(data) {
         $('#movieList').empty().append('<option value="" disabled selected>Select a movie</option>');
@@ -13,9 +27,11 @@ $(document).ready(function() {
         if (movieId) {
             $.get(`/places/${movieId}`, function(moviePlace) {
                 $('#locationList').empty();
+                const hobbiesReadable = bitwiseToReadable(moviePlace.hobby, hobbies);
+                const favoriteFoodsReadable = bitwiseToReadable(moviePlace.favoriteFood, favoriteFoods);
                 $('#locationList').append('<li>Name: ' + moviePlace.name + '</li>');
-                $('#locationList').append('<li>Hobby: ' + moviePlace.hobby + '</li>');
-                $('#locationList').append('<li>Favorite Food: ' + moviePlace.favoriteFood + '</li>');
+                $('#locationList').append('<li>Hobby: ' + hobbiesReadable + '</li>');
+                $('#locationList').append('<li>Favorite Food: ' + favoriteFoodsReadable + '</li>');
             })
         }
     });
